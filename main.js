1ed8adf4db4380f12bc764f43509aea2543b6fd9
Lennon Fonseca <lennonfonsecaa@gmail.com>
2024-01-19 11:29:15 -0300

    add assessment

---
+++ main.js
@@ -0,0 +1,113 @@
+// 1 - Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// alert('Hello World!')
+
+
+// 2 - Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// let numberOne = Number(prompt('digite um número: '))
+// let numberTwo = Number(prompt('digite outro número: '))
+
+// function sum() {
+//     result = numberOne + numberTwo
+// }
+
+// sum()
+// alert(`o Resultado da soma é ${result}`)
+
+
+// 3 - Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+// let isNumber = "h"
+
+
+// if (typeof isNumber == 'number') {
+//     alert('é um número')
+// } else {
+//     alert('não é um número')
+// }
+
+
+// 4 - Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// let isString = 9
+
+// if (typeof isString == 'string') {
+//     alert(`o ${isString} é uma string!`)
+// } else {
+//     alert(`o ${isString} não é uma string!`)
+// }
+
+
+// 5 - Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// let isBoolean = false
+
+// if(typeof isBoolean == 'boolean'){
+//     alert(`o ${isBoolean} é um booleano!`)
+// } else {
+//     alert(`o ${isBoolean} não é um booleano!`)
+// }
+
+
+// 6 - Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// alert('Digite dois números para uma conta de subtração...')
+
+// let numberOne = Number(prompt('digite o primeiro número: '))
+// let numberTwo = Number(prompt('digite o segundo número: '))
+
+// let result = numberOne - numberTwo
+
+// alert(`o resultado é: ${result}`)
+
+
+// 7 - Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+
+// alert('Digite dois números para uma conta de multiplicação...')
+
+// let numberOne = Number(prompt('digite o primeiro número: '))
+// let numberTwo = Number(prompt('digite o segundo número: '))
+
+// let result = numberOne * numberTwo
+
+// alert(`o resultado é: ${result}`)
+
+
+// 8 - Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// alert('Digite dois números para uma conta de divisão...')
+
+// let numberOne = Number(prompt('digite o primeiro número: '))
+// let numberTwo = Number(prompt('digite o segundo número: '))
+
+// let result = numberOne / numberTwo
+
+// alert(`o resultado é: ${result}`)
+
+
+// 9 - Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// let numberOne = 4
+
+// result = numberOne %  2
+
+// if(result == 0) {
+//     alert(`o ${numberOne} é um número par!`)
+// } else {
+//     alert(`o ${numberOne} não é um número par`)
+// }
+
+
+// 10 - Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+let numberOne = 5
+
+result = numberOne %  2
+
+if(result != 0) {
+    alert(`o ${numberOne} é um número ímpar!`)
+} else {
+    alert(`o ${numberOne} não é um número ímpar`)
+}
